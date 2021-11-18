9f5c01bdd56fc98beb1fd2772cad2900d3c8cf37
Saheethi <89166758+Saheethi@users.noreply.github.com>
2021-11-17 18:58:02 -0600

    part 5  of assignment 12

--- controllers/fan.js
+++ controllers/fan.js
@@ -103,3 +103,17 @@ exports.fan_view_one_Page = async function(req, res) {
     } 
 }; 
 
+// Handle building the view for creating a fan. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.fan_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('fancreate', { title: 'fan Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
--- routes/fan.js
+++ routes/fan.js
@@ -12,6 +12,8 @@ router.get('/detail', fan_controller.fan_view_one_Page);
 
 /* GET create fan page */ 
 router.get('/create', fan_controller.fan_create_Page);
+
+
  
 module.exports = router;
 
